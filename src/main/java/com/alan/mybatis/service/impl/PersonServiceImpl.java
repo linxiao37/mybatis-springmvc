509c5bca6579268d207482f491314392ef161bf5
xiaolin35 <xiaolin35@jd.com>
2020-08-28 10:37:04 +0800

    test

--- src/main/java/com/alan/mybatis/service/impl/PersonServiceImpl.java
+++ src/main/java/com/alan/mybatis/service/impl/PersonServiceImpl.java
@@ -4,10 +4,13 @@ import com.alan.mybatis.mapper.PersonMapper;
 import com.alan.mybatis.model.Person;
 import com.alan.mybatis.model.PersonExample;
 import com.alan.mybatis.service.IPersonService;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -18,11 +21,14 @@ import java.util.List;
 @Service
 public class PersonServiceImpl implements IPersonService {
 
-    @Autowired
-    private PersonMapper personMapper;
+    //@Autowired
+    //private PersonMapper personMapper;
+    @Resource
+    private SqlSessionTemplate sqlSessionTemplate;
 
     @Override
     public List<Person> selectList() {
+        PersonMapper personMapper = sqlSessionTemplate.getMapper(PersonMapper.class);
         PersonExample example = new PersonExample();
         return personMapper.selectByExample(example);
     }
